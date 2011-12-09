903b5035383856986583a1fe24029e2228eaf93b
Muhammad Abbas <mabbas.p@gmail.com>
2011-12-09 16:22:45 +0800

    first commit

---
+++ src/com/testing/helloandroid/HelloworldActivity.java
@@ -0,0 +1,13 @@
+package com.testing.helloandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelloworldActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
